202cae8d743aa707139fd2e3df2d9f4972c61424
Aditya-Vaibhav <adityavaibhav139@gmail.com>
2025-12-25 15:43:16 +0530

    Add Post model with schema definition for title, description, images, videos, likes, comments, and author

---
+++ back-end/src/models/posts.js
@@ -0,0 +1,53 @@
+import mongoose,{Schema} from 'mongoose';
+
+const postSchema = new Schema({
+    title:{
+        type: String,
+        required: true,
+        trim: true
+    },
+
+    description:{
+        type: String,
+        trim: true
+    },
+
+    postImage: {
+        type: String,
+        trim: true
+    },
+
+    postVideo: {
+        type: String,
+        trim: true
+    },
+
+    likedBy: [{
+        type: Schema.Types.ObjectId,
+        ref: 'User'
+    }],
+
+    likeCount: {
+        type: Number,
+        default: 0
+    },
+
+    comments: [{
+        type: Schema.Types.ObjectId,
+        ref: 'Comment'
+    }],
+    
+    commentsCount: {
+        type: Number,
+        default: 0
+    },
+
+    author: {
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    }
+
+},{timestamps: true});
+
+export const Post = mongoose.model('Post',postSchema)

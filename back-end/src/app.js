187515faeb234e6bebd8f445b28290f8976cbb49
Aditya-Vaibhav <adityavaibhav139@gmail.com>
2025-12-27 09:03:58 +0530

    Initialize Express app with JSON parsing and URL encoding; set up API routes for users, posts, comments, and likes

--- back-end/src/app.js
+++ back-end/src/app.js
@@ -2,6 +2,10 @@ import express from 'express'
 
 const app = express();
 
+
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+
 app.get('/',(req,res)=>{
     res.send('Main page');
 })
@@ -9,6 +13,14 @@ app.get('/',(req,res)=>{
 
 
 //routes
+import userRoutes from './routes/user.route.js';
+import postRoutes from './routes/post.route.js';
+import commentRoutes from './routes/comment.route.js';
+import likeRoutes from './routes/like.route.js';
 
+app.use('/api/v1/users',userRoutes);
+app.use('/api/v1/posts',postRoutes);
+app.use('/api/v1/comments',commentRoutes);
+app.use('/api/v1/likes',likeRoutes);
 
 export default app;

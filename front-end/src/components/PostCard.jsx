81a1fe4bea64a4ff79f0c01bdb60e8fe53faf32f
Aditya-Vaibhav <adityavaibhav139@gmail.com>
2026-01-04 11:50:08 +0530

    Enhance PostCard component to display videos and fix image rendering; remove PostDetails page

--- front-end/src/components/PostCard.jsx
+++ front-end/src/components/PostCard.jsx
@@ -2,6 +2,32 @@ const PostCard = ({post,onLike})=>{
     return(
         <div>
             <h3>post.title</h3>
+
+            {
+                post.postImages?.length > 0 &&
+                    post.postImages.map((img,index)=>{
+                        <img
+                            key={index}
+                            src={img}
+                            alt={`post-img-${index}`}
+                        />
+                    })
+                
+            }
+
+            {
+                post.postVideos?.length > 0 &&
+                    post.postVideos.map((video,index)=>{
+                        <video
+                            key={index}
+                            src={video}
+                            alt={`post-img-${index}`}
+                        />
+                    })
+                
+            }
+
+
             <p>post.description</p>
             
             <button onClick={()=>{onLike(post._id)}}>❤️{post.likeCount}</button>
